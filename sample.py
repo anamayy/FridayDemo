79372eb450038fd0a4c9eb2f0aaca5d63b3a7770
anamayy <anamaysingh46@gmail.com>
2025-02-06 15:39:09 +0530

    changed code

--- sample.py
+++ sample.py
@@ -1,8 +1,5 @@
 def sum_natural(n):
-    total = 0
-    for i in range(1, n + 1):
-        total += i
-    return total
+    return n * (n + 1) // 2
 
 n = int(input("Enter a number: "))
 print("Sum:", sum_natural(n))
