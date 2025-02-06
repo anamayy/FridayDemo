dded1373b1a39b4ced12a9269d5f08cabb1928b1
anamayy <anamaysingh46@gmail.com>
2025-02-06 15:00:14 +0530

    added formula method to add natural numbers

--- sample.py
+++ sample.py
@@ -1,8 +1,5 @@
 def sum_natural_loop(n):
-    total = 0
-    for i in range(1, n + 1):
-        total += i
-    return total
+    return n * (n + 1) // 2
 
 n = int(input("Enter a number: "))
 print("Sum:", sum_natural_loop(n))
